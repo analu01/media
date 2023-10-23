f578a5767bf17630e7305b26f54d93b3aa8b5fc7
analu01 <113209008+analu01@users.noreply.github.com>
2023-10-23 07:54:20 -0300

    23/10

--- triangulo.js
+++ triangulo.js
@@ -1,41 +1,32 @@
 const form = document.querySelector("form")
-const resp1 = document.querySelector("#outResp")
-const resp2 = document.querySelector("#outResp1")
+const resp = document.querySelector("#outResp")
+const resp1 = document.querySelector("#outResp1")
 
 form.addEventListener("submit", (e) => {
-e.preventDefault()
- 
+    e.preventDefault()
 
-  const LadoA = Number(form.inLadoA.value)
-  const LadoB = Number(form.inLadoB.value)
-  const LadoC = Number(form.inLadoC.value)
+    const ladoA = Number(form.inLadoA.value)
+    const ladoB = Number(form.inLadoB.value)
+    const ladoC = Number(form.inLadoC.value)
 
+    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
+        alert("Essas medidas não podemformar um triângulo. Um lado não pode ser maior que a soma dos outros dois.")
+        form.inLadoA.focus()
+        form.inLadoB.focus()
+        form.inLadoC.focus()
+        return
+    }
 
-  if((LadoA > LadoB + LadoC) || (LadoB > LadoA + LadoC ) (LadoC > LadoA + LadoB)){
-      alert("Essas medidas não podem formar um triângulo.Um dos lados não pode ser maior que a some dos outros dois. ")
-      form.inLadoA.focus()
-      form.inLadoB.focus()
-      form.inLadoC.focus()
-      return
-  }
+    resp.innerText = `Lados podem formar um triângulo`
 
-  resp.innerText = `Lados podem formar um triângulo`
-
-  if(LadoA == LadoB && LadoA == LadoC){
-    resp.innerText =`Tipo : Equilátero`
-
-} else if((LadoA == LadoB)|| (LadoA == LadoC) || (LadoB == LadoC)){
-    resp.innerText = `Tipo : Isóceles`
-
-}  else {
-    resp.innerText = `Tipo : Escaleno`
-}
-
-
+    if(ladoA == ladoB && ladoA == ladoC){
+        resp1.innerText = `Tipo: Equilátero`
+    } else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)){
+        resp1.innerText = `Tipo: Isóceles`
+    }else{
+        resp1.innerText = `Tipo: Escaleno`
+    }
 })
-
-
-
 
 
 
