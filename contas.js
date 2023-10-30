518f8afcaf2a21b8f4393ee50aa4d3cb0e4e34ae
analu01 <113209008+analu01@users.noreply.github.com>
2023-10-30 08:17:28 -0300

    30/10

---
+++ contas.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("#outResp1")
+const resp1 = document.querySelector("#outResp2")
+
+let numContas = 0
+let valorTotal = 0
+let resposta = ""
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+const descrição = frm.inDescrição.value
+const valor = Number(frm.inValor.value)
+
+numContas++
+valTotal = valTotal + valor
+
+resposta = resposta + descrição + " - R$: " + valor.toFixed(2) + "\n"
+
+resp1.innerText = `${resposta}--------------------------------`
+resp1.innerText = `${numContas} Conta(s) - Total R$: ${valTotal.toFixed(2)} `
+
+frm.inDescrição.value = ""
+frm.inValor.value = ""
+frm.inDescrição.value.focus()
+})
